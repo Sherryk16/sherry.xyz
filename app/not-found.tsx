afc2d6c31e70e4230f7212ef593ec0498c55b4e6
Sheharyar Khan <sherry258012@gmail.com>
2025-06-04 23:12:42 +0500

    e

--- app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="flex h-screen flex-col items-center justify-center text-center">
-      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
-      <p className="mt-4 text-gray-500">Sorry, the page you're looking for doesn't exist.</p>
-    </div>
-  )
-}
