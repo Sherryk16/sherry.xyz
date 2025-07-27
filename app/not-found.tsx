8c3e4ad5f71d1055b78e55e6f2a3b6f36487b689
Sheharyar Khan <sherry258012@gmail.com>
2025-07-27 20:33:52 +0500

    hh

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,54 @@
-'use client'
+import Link from '@/components/Link'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import { auth } from '@/auth'
+import Image from 'next/image'
+import GIF from '@/public/static/gifs/confused-travolta.gif'
+import { cn } from '@/lib/utils'
+import { buttonVariants, Button } from '@/components/ui/button'
+import SiteLogo from '@/components/SiteLogos'
 
-import Link from 'next/link'
-import { buttonVariants } from '@/components/ui/button'
+import { genPageMetadata } from 'app/seo'
+export const metadata = genPageMetadata({ title: 'Check Google Maps!' })
 
-export default function NotFound() {
+export default async function NotFound() {
+  const session = await auth()
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-16">
-      <h1 className="mb-4 text-4xl font-bold">404 - Page Not Found</h1>
-      <p className="mb-8 text-lg">Sorry, the page you are looking for does not exist.</p>
-      <Link href="/" className={buttonVariants({ variant: 'outline' })}>
-        Return Home
-      </Link>
-    </div>
+    <>
+      <Header session={session} />
+      <main className="mb-auto pt-32">
+        <div className="mb-auto flex flex-col content-center space-y-10">
+          {/* <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 ">
+            <div className="space-x-2 pb-8 pt-6 md:space-y-5">
+              <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
+                404
+              </h1>
+            </div>
+            <div className="max-w-md">
+              <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
+                Sorry we couldn't find this page.
+              </p>
+              <p className="mb-8">
+                But dont worry, you can find plenty of other things on our homepage.
+              </p>
+            </div>
+          </div> */}
+
+          <div className="mx-auto mt-5">
+            <SiteLogo kind={'logo'} size={15} logoType="image" parentClassName={'mx-auto'} />
+          </div>
+          <Image src={GIF} alt="Page Not Found GIF Wink" className="mx-auto mt-16 "></Image>
+          <p className="mx-auto mb-4 text-xl font-bold leading-normal md:text-2xl">
+            Lost your way?
+          </p>
+          <Button asChild className={cn(buttonVariants({ variant: 'secondary' }), 'mx-auto')}>
+            <Link href="/">Back to homepage</Link>
+          </Button>
+        </div>
+      </main>
+
+      <Footer />
+    </>
   )
 }
