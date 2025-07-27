00b15d2570b080bec2d83824281e97f5cf3f2159
Sheharyar Khan <sherry258012@gmail.com>
2025-07-27 17:41:42 +0500

    l

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,9 +7,9 @@ export default function NotFound() {
   return (
     <div className="pt-32 text-center">
       <Image src={GIF} alt="Page Not Found" className="mx-auto" />
-      <h1 className="text-4xl font-bold mt-8">Lost your way?</h1>
-      <p className="text-lg mt-2">Don't worry, go back to homepage.</p>
-      <Link href="/" className="mt-6 inline-block bg-gray-800 text-white py-2 px-4 rounded">
+      <h1 className="mt-8 text-4xl font-bold">Lost your way?</h1>
+      <p className="mt-2 text-lg">Don't worry, go back to homepage.</p>
+      <Link href="/" className="mt-6 inline-block rounded bg-gray-800 px-4 py-2 text-white">
         Go Home
       </Link>
     </div>
